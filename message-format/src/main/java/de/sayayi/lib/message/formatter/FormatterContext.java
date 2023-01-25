927f0f90c06ccfa83de1fbaf8556f92bf2b5db7a
jeroen.gremmen <jeroen.gremmen@sayayi.de>
2023-01-26 00:01:04 +0100

    add javadoc

--- message-format/src/main/java/de/sayayi/lib/message/formatter/FormatterContext.java
+++ message-format/src/main/java/de/sayayi/lib/message/formatter/FormatterContext.java
@@ -36,8 +36,14 @@ import java.util.Set;
  * @author Jeroen Gremmen
  * @since 0.8.0
  */
+@SuppressWarnings("unused")
 public interface FormatterContext extends Parameters
 {
+  /**
+   * Returns the message context used to create this formatter context.
+   *
+   * @return  message context, never {@code null}
+   */
   @Contract(pure = true)
   @NotNull MessageContext getMessageContext();
 
@@ -81,31 +87,95 @@ public interface FormatterContext extends Parameters
   @NotNull OptionalBoolean getConfigValueBool(@NotNull String name);
 
 
+  /**
+   * <p>
+   *   Delegate formatting to next best parameter formatter.
+   * </p>
+   * <p>
+   *   Based on the object type, format and registered formatters, a list of prioritized formatters
+   *   is calculated and the top formatter is invoked. If that formatter delegates formatting, the
+   *   next formatter from the list will be invoked with the same formatter context.
+   * </p>
+   * <p>
+   *   The last formatter from the list, which is usually the formatter associated with type
+   *   {@code Object}, must not delegate to the next formatter or a {@code NoSuchElementException}
+   *   is thrown. By default the {@code Object} formatter is a string formatter, which will never
+   *   delegate. However, if this formatter is redefined, it must never delegate formatting.
+   * </p>
+   * <p>
+   *   A named formatter never has a next formatter it can delegate to.
+   * </p>
+   *
+   * @return  formatted text, never {@code null}
+   */
   @NotNull Text delegateToNextFormatter();
 
 
+  /**
+   * Format the given {@code value} using its type.
+   *
+   * @param value  value to format
+   *
+   * @return  formatted text, never {@code null}
+   */
   @Contract(pure = true)
   default @NotNull Text format(Object value) {
     return format(value, null, false);
   }
 
 
+  /**
+   * Format the given {@code value} using its type and the current format designator
+   * (if available and if {@code propagateFormat = true}).
+   *
+   * @param value            value to format
+   * @param propagateFormat  propagate parameter format designator if {@code true}, ignore format
+   *                         designator if this parameter is {@code false}
+   *
+   * @return  formatted text, never {@code null}
+   */
   @Contract(pure = true)
   default @NotNull Text format(Object value, boolean propagateFormat) {
     return format(value, null, propagateFormat);
   }
 
 
+  /**
+   * Format the given {@code value} using {@code type}.
+   *
+   * @param value            value to format
+   * @param type             value type or {@code null}
+   *
+   * @return  formatted text, never {@code null}
+   */
   @Contract(pure = true)
   default @NotNull Text format(Object value, @NotNull Class<?> type) {
     return format(value, type, false);
   }
 
 
+  /**
+   * Format the given {@code value} using {@code type} and the current format designator
+   * (if available and if {@code propagateFormat = true}).
+   *
+   * @param value            value to format
+   * @param type             value type or {@code null}
+   * @param propagateFormat  propagate parameter format designator if {@code true}, ignore format
+   *                         designator if this parameter is {@code false}
+   *
+   * @return  formatted text, never {@code null}
+   */
   @Contract(pure = true)
   @NotNull Text format(Object value, Class<?> type, boolean propagateFormat);
 
 
+  /**
+   * Format the given {@code message}.
+   *
+   * @param message  message, or {@code null}
+   *
+   * @return  formatted text, never {@code null}
+   */
   @Contract(pure = true)
   @NotNull Text format(Message.WithSpaces message);
 }
