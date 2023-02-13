a6b0431217157d32264aac76ea3c2c000f1eb6c6
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2023-02-13 14:36:12 +0100

    add Key formatter

---
+++ message-format/src/main/java/de/sayayi/lib/message/formatter/runtime/KeyFormatter.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2023 Jeroen Gremmen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.sayayi.lib.message.formatter.runtime;
+
+import de.sayayi.lib.message.formatter.AbstractParameterFormatter;
+import de.sayayi.lib.message.formatter.FormattableType;
+import de.sayayi.lib.message.formatter.FormatterContext;
+import de.sayayi.lib.message.internal.part.MessagePart.Text;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.security.Key;
+import java.util.Set;
+
+import static de.sayayi.lib.message.internal.part.MessagePartFactory.noSpaceText;
+import static de.sayayi.lib.message.internal.part.MessagePartFactory.nullText;
+import static java.util.Collections.singleton;
+
+
+/**
+ * @author Jeroen Gremmen
+ * @since 0.8.0
+ */
+public final class KeyFormatter extends AbstractParameterFormatter
+{
+  @Override
+  protected @NotNull Text formatValue(@NotNull FormatterContext formatterContext, Object value)
+  {
+    if (value == null)
+      return nullText();
+
+    final Key key = (Key)value;
+
+    switch(formatterContext.getConfigValueString("key").orElse("name"))
+    {
+      case "algorithm":
+        return noSpaceText(key.getAlgorithm());
+
+      case "format":
+        return noSpaceText(key.getFormat());
+    }
+
+    return formatterContext.delegateToNextFormatter();
+  }
+
+
+  @Override
+  @Contract(value = "-> new", pure = true)
+  public @NotNull Set<FormattableType> getFormattableTypes() {
+    return singleton(new FormattableType(Key.class));
+  }
+}
