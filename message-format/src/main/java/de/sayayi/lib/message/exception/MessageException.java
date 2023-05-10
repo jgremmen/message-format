a9ec81de391fd512bde50afd5fc2a291d64fef81
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2023-05-10 15:34:08 +0200

    add documentation

--- message-format/src/main/java/de/sayayi/lib/message/exception/MessageException.java
+++ message-format/src/main/java/de/sayayi/lib/message/exception/MessageException.java
@@ -43,7 +43,7 @@ public class MessageException extends RuntimeException
    * @param message  the detail message (which is saved for later retrieval by the
    *                 {@link #getMessage()} method).
    * @param cause    the cause (which is saved for later retrieval by the {@link #getCause()}
-   *                 method). (A <tt>null</tt> value is permitted, and indicates that the cause is
+   *                 method). (A {@code null} value is permitted, and indicates that the cause is
    *                 nonexistent or unknown.)
    */
   public MessageException(String message, Throwable cause) {
