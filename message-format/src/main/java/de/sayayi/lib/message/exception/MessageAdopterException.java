2ab4c5165e23bd34f2d3d7535881fcd5e99e3958
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2023-05-10 15:30:34 +0200

    add documentation

--- message-format/src/main/java/de/sayayi/lib/message/exception/MessageAdopterException.java
+++ message-format/src/main/java/de/sayayi/lib/message/exception/MessageAdopterException.java
@@ -35,6 +35,18 @@ public class MessageAdopterException extends MessageException
   }
 
 
+  /**
+   * Constructs a new message adapter exception with the specified detail message and cause.
+   * <p>
+   * Note that the detail message associated with {@code cause} is <i>not</i> automatically
+   * incorporated in this runtime exception's detail message.
+   *
+   * @param message  the detail message (which is saved for later retrieval by the
+   *                 {@link #getMessage()} method).
+   * @param cause    the cause (which is saved for later retrieval by the {@link #getCause()}
+   *                 method). (A <tt>null</tt> value is permitted, and indicates that the cause is
+   *                 nonexistent or unknown.)
+   */
   public MessageAdopterException(String message, Throwable cause) {
     super(message, cause);
   }
