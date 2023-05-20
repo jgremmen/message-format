6bf94840f54d5e3029d3ab3beeefcba8f02c2fea
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2023-05-20 22:11:43 +0200

    add byte formatter test case

--- message-format/src/main/java/de/sayayi/lib/message/formatter/runtime/ByteArrayFormatter.java
+++ message-format/src/main/java/de/sayayi/lib/message/formatter/runtime/ByteArrayFormatter.java
@@ -19,7 +19,6 @@ import de.sayayi.lib.message.formatter.AbstractParameterFormatter;
 import de.sayayi.lib.message.formatter.FormattableType;
 import de.sayayi.lib.message.formatter.FormatterContext;
 import de.sayayi.lib.message.formatter.ParameterFormatter.EmptyMatcher;
-import de.sayayi.lib.message.formatter.ParameterFormatter.SizeQueryable;
 import de.sayayi.lib.message.internal.part.MessagePart.Text;
 import de.sayayi.lib.message.parameter.key.ConfigKey.CompareType;
 import de.sayayi.lib.message.parameter.key.ConfigKey.MatchResult;
@@ -27,10 +26,11 @@ import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.UnsupportedEncodingException;
-import java.util.OptionalLong;
+import java.util.Optional;
 import java.util.Set;
 
 import static de.sayayi.lib.message.internal.part.MessagePartFactory.emptyText;
+import static de.sayayi.lib.message.internal.part.MessagePartFactory.nullText;
 import static de.sayayi.lib.message.parameter.key.ConfigKey.MatchResult.TYPELESS_EXACT;
 import static java.nio.charset.Charset.isSupported;
 import static java.util.Collections.singleton;
@@ -40,35 +40,33 @@ import static java.util.Collections.singleton;
  * @author Jeroen Gremmen
  * @since 0.8.0
  */
-public final class ByteArrayFormatter extends AbstractParameterFormatter
-    implements EmptyMatcher, SizeQueryable
+public final class ByteArrayFormatter extends AbstractParameterFormatter implements EmptyMatcher
 {
   @Override
   @SneakyThrows(UnsupportedEncodingException.class)
   public @NotNull Text formatValue(@NotNull FormatterContext context, Object byteArray)
   {
-    final byte[] bytes = (byte[])byteArray;
-    if (bytes == null || bytes.length == 0)
-      return emptyText();
-
-    final String charset = context.getConfigValueString("charset").orElse("");
-    if (charset.isEmpty())
+    final Optional<String> charsetConfig = context.getConfigValueString("charset");
+    if (!charsetConfig.isPresent())
       return context.delegateToNextFormatter();
 
-    return context.format(isSupported(charset)
-        ? new String(bytes, charset) : new String(bytes), true);
+    if (byteArray == null)
+      return nullText();
+
+    final byte[] bytes = (byte[])byteArray;
+    if (bytes.length == 0)
+      return emptyText();
+
+    final String charset = charsetConfig.get();
+
+    return context.format(charset.isEmpty() || !isSupported(charset)
+        ? new String(bytes) : new String(bytes, charset), true);
   }
 
 
   @Override
   public MatchResult matchEmpty(@NotNull CompareType compareType, @NotNull Object value) {
     return compareType.match(((byte[])value).length) ? TYPELESS_EXACT : null;
-  }
-
-
-  @Override
-  public @NotNull OptionalLong size(@NotNull FormatterContext context, @NotNull Object value) {
-    return OptionalLong.of(((byte[])value).length);
   }
 
 
---
+++ message-format/src/test/java/de/sayayi/lib/message/formatter/runtime/ByteArrayFormatterTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2023 Jeroen Gremmen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.sayayi.lib.message.formatter.runtime;
+
+import de.sayayi.lib.message.MessageSupportFactory;
+import de.sayayi.lib.message.formatter.AbstractFormatterTest;
+import de.sayayi.lib.message.internal.part.TextPart;
+import de.sayayi.lib.message.parameter.key.ConfigKeyName;
+import de.sayayi.lib.message.parameter.value.ConfigValueString;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.IllegalCharsetNameException;
+
+import static de.sayayi.lib.message.MessageFactory.NO_CACHE_INSTANCE;
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonMap;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+
+/**
+ * @author Jeroen Gremmen
+ * @since 0.8.0
+ */
+public class ByteArrayFormatterTest extends AbstractFormatterTest
+{
+  @Test
+  void testFormattableTypes() {
+    assertFormatterForType(new ByteArrayFormatter(), byte[].class);
+  }
+
+
+  @Test
+  @SuppressWarnings({"ConvertToBasicLatin", "SpellCheckingInspection"})
+  void testEncodedByteArray()
+  {
+    val formatterService = createFormatterService(new ByteArrayFormatter(), new ArrayFormatter());
+    val messageAccessor = MessageSupportFactory.create(formatterService, NO_CACHE_INSTANCE)
+        .setLocale("de-DE")
+        .getMessageAccessor();
+
+    assertEquals(new TextPart("Größe"),
+        format(messageAccessor, "Größe".getBytes(ISO_8859_1),
+            singletonMap(new ConfigKeyName("charset"), new ConfigValueString("iso-8859-1"))));
+    assertEquals(new TextPart("Größe"),
+        format(messageAccessor, "Größe".getBytes(UTF_8),
+            singletonMap(new ConfigKeyName("charset"), new ConfigValueString("utf-8"))));
+    assertEquals(new TextPart("Größe"),
+        format(messageAccessor, "Größe".getBytes(),
+            singletonMap(new ConfigKeyName("charset"), new ConfigValueString(""))));
+    assertEquals(new TextPart("Größe"),
+        format(messageAccessor, "Größe".getBytes(),
+            singletonMap(new ConfigKeyName("charset"), new ConfigValueString("AA-bb"))));
+  }
+
+
+  @Test
+  void testIllegalCharset()
+  {
+    val formatterService = createFormatterService(new ByteArrayFormatter(), new ArrayFormatter());
+    val messageAccessor = MessageSupportFactory.create(formatterService, NO_CACHE_INSTANCE)
+        .setLocale("de-DE")
+        .getMessageAccessor();
+
+    assertThrowsExactly(IllegalCharsetNameException.class, () ->
+        format(messageAccessor, "Größe".getBytes(),
+            singletonMap(new ConfigKeyName("charset"), new ConfigValueString("XYZ&%"))));
+  }
+
+
+  @Test
+  void testDelegate()
+  {
+    val formatterService = createFormatterService(new ByteArrayFormatter(), new ArrayFormatter());
+    val messageAccessor = MessageSupportFactory.create(formatterService, NO_CACHE_INSTANCE)
+        .setLocale("de-DE")
+        .getMessageAccessor();
+
+    assertEquals(new TextPart("71, 114, -61, -74, -61, -97, 101"),
+        format(messageAccessor, "Größe".getBytes(UTF_8), emptyMap()));
+  }
+}
