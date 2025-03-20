3a960f93bd0f420c38d624823c89ac1d180d67a7
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2025-03-20 07:38:51 +0100

    improve MessageCompiler test cases

--- message-format/src/test/java/de/sayayi/lib/message/internal/parser/MessageCompilerTest.java
+++ message-format/src/test/java/de/sayayi/lib/message/internal/parser/MessageCompilerTest.java
@@ -21,9 +21,7 @@ import de.sayayi.lib.message.part.MessagePart;
 import de.sayayi.lib.message.part.normalizer.LRUMessagePartNormalizer;
 import de.sayayi.lib.message.part.parameter.ParameterConfig;
 import de.sayayi.lib.message.part.parameter.ParameterPart;
-import de.sayayi.lib.message.part.parameter.key.ConfigKeyEmpty;
-import de.sayayi.lib.message.part.parameter.key.ConfigKeyName;
-import de.sayayi.lib.message.part.parameter.key.ConfigKeyNull;
+import de.sayayi.lib.message.part.parameter.key.*;
 import de.sayayi.lib.message.part.parameter.value.ConfigValueBool;
 import de.sayayi.lib.message.part.parameter.value.ConfigValueMessage;
 import de.sayayi.lib.message.part.parameter.value.ConfigValueNumber;
@@ -34,6 +32,7 @@ import java.util.Map;
 
 import static de.sayayi.lib.message.exception.MessageParserException.Type.MESSAGE;
 import static de.sayayi.lib.message.part.TextPartFactory.*;
+import static de.sayayi.lib.message.part.parameter.key.ConfigKey.CompareType.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -233,5 +232,89 @@ class MessageCompilerTest
             )))
         },
         COMPILER.compileMessage("%{ p, <>empty:'msg %{n}' }").getMessageParts());
+  }
+
+
+  @Test
+  @DisplayName("Parameter with boolean configuration key")
+  void testParameterWithBooleanKey()
+  {
+    assertArrayEquals(
+        new MessagePart[] {
+            new ParameterPart("p", new ParameterConfig(Map.of(
+                ConfigKeyBool.TRUE, new ConfigValueString("msg")
+            )))
+        },
+        COMPILER.compileMessage("%{ p, true:msg }").getMessageParts());
+
+    assertArrayEquals(
+        new MessagePart[] {
+            new ParameterPart("p", new ParameterConfig(Map.of(
+                ConfigKeyBool.FALSE, new ConfigValueMessage(COMPILER.compileMessage("msg %{n}"))
+            )))
+        },
+        COMPILER.compileMessage("%{ p, false:'msg %{n}' }").getMessageParts());
+  }
+
+
+  @Test
+  @DisplayName("Parameter with number configuration key")
+  void testParameterWithNumberKey()
+  {
+    assertArrayEquals(
+        new MessagePart[] {
+            new ParameterPart("p", new ParameterConfig(Map.of(
+                new ConfigKeyNumber(EQ, 16), new ConfigValueString("msg1")
+            )))
+        },
+        COMPILER.compileMessage("%{ p, 16:msg1 }").getMessageParts());
+
+    assertArrayEquals(
+        new MessagePart[] {
+            new ParameterPart("p", new ParameterConfig(Map.of(
+                new ConfigKeyNumber(EQ, -16), new ConfigValueMessage(COMPILER.compileMessage(" msg 2 "))
+            )))
+        },
+        COMPILER.compileMessage("%{ p, =-16:\" msg 2 \" }").getMessageParts());
+
+    assertArrayEquals(
+        new MessagePart[] {
+            new ParameterPart("p", new ParameterConfig(Map.of(
+                new ConfigKeyNumber(LT, 1000), new ConfigValueString("msg3")
+            )))
+        },
+        COMPILER.compileMessage("%{ p, < 1000:msg3 }").getMessageParts());
+
+    assertArrayEquals(
+        new MessagePart[] {
+            new ParameterPart("p", new ParameterConfig(Map.of(
+                new ConfigKeyNumber(LTE, 0), new ConfigValueString("msg4")
+            )))
+        },
+        COMPILER.compileMessage("%{ p,<=0:msg4 }").getMessageParts());
+
+    assertArrayEquals(
+        new MessagePart[] {
+            new ParameterPart("p", new ParameterConfig(Map.of(
+                new ConfigKeyNumber(NE, 1), new ConfigValueString("msg5")
+            )))
+        },
+        COMPILER.compileMessage("%{ p,<>1:msg5 }").getMessageParts());
+
+    assertArrayEquals(
+        new MessagePart[] {
+            new ParameterPart("p", new ParameterConfig(Map.of(
+                new ConfigKeyNumber(GT, 123456789), new ConfigValueString("msg6")
+            )))
+        },
+        COMPILER.compileMessage("%{ p,>123456789:msg6 }").getMessageParts());
+
+    assertArrayEquals(
+        new MessagePart[] {
+            new ParameterPart("p", new ParameterConfig(Map.of(
+                new ConfigKeyNumber(GTE, -987654321), new ConfigValueMessage(COMPILER.compileMessage(" msg 7"))
+            )))
+        },
+        COMPILER.compileMessage("%{ p, >= -987654321:\" msg 7\" }").getMessageParts());
   }
 }
