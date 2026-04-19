f32d76e528f5f4dcdcfda9d92aabb3bf8a7a1e26
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2026-04-19 10:12:41 +0200

    doc: Add package-info.java files for message format library components

---
+++ message-format/src/main/java/de/sayayi/lib/message/formatter/parameter/named/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2019 Jeroen Gremmen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Named parameter formatter implementations that can be explicitly referenced by name in a message format, such as
+ * {@code bool}, {@code choice}, {@code size} and {@code string}.
+ *
+ * @author Jeroen Gremmen
+ * @since 0.1.0
+ */
+package de.sayayi.lib.message.formatter.parameter.named;
---
+++ message-format/src/main/java/de/sayayi/lib/message/internal/part/typedvalue/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2020 Jeroen Gremmen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal implementations of {@link de.sayayi.lib.message.part.TypedValue TypedValue} for the
+ * various value types: string, number, boolean and message.
+ *
+ * @author Jeroen Gremmen
+ * @since 0.5.0
+ */
+package de.sayayi.lib.message.internal.part.typedvalue;
